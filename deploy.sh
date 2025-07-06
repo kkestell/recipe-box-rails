f48d3fc3fb5da6a0b22e206940200f4b9076a22a
Kyle Kestell <kyle@kestell.org>
2025-07-05 21:57:47 -0500

    Rename project.

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,7 @@ ssh kkestell_smidge@ssh.nyc1.nearlyfreespeech.net << 'EOF'
   set -e
   cd /home/protected/smidge
   git pull
-  bundle config set path 'vendor/bundle'
-  bundle config set without 'development test'
-  bundle install
+  MAKE=gmake bundle install
   RAILS_ENV=production SECRET_KEY_BASE_DUMMY=1 bundle exec rails assets:precompile
   RAILS_ENV=production bundle exec rails db:migrate
   bundle exec rails restart
